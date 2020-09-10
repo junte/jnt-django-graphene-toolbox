ceee219ed5d0393f89686518a797bd10bd9babad
Stanislav Lapshin <staslapshin88@gmail.com>
2020-09-10 14:29:48 +0300

    [#] Refactor erros folders

--- src/jnt_django_graphene_toolbox/errors/input.py
+++ src/jnt_django_graphene_toolbox/errors/input.py
@@ -1,58 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from copy import deepcopy
 from typing import Any, Dict, List, Optional, Sequence
 
 from django.utils.translation import gettext_lazy as _
 from graphene_django.utils import camelize
-from graphql import GraphQLError
 
-ACCESS_DENIED = "ACCESS_DENIED"
-NOT_FOUND = "NOT_FOUND"
+from jnt_django_graphene_toolbox.errors.base import BaseGraphQLError
+
 INPUT_ERROR = "INPUT_ERROR"
-AUTHENTICATION_FAILED = "AUTHENTICATION_FAILED"
-
-
-class BaseGraphQLError(GraphQLError):
-    """Base class for GraphQL errors."""
-
-    _default_message: str = "Error is occurred"
-    _default_extensions: Optional[Dict[str, str]] = None
-
-    def __init__(self, message=None, extensions=None, *args, **kwargs) -> None:
-        """Extending class with default message and extensions."""
-        if not message:
-            message = self._default_message
-
-        if self._default_extensions:
-            merged_extensions = deepcopy(self._default_extensions)
-            if extensions:
-                merged_extensions.update(extensions)
-            extensions = merged_extensions
-
-        kwargs["message"] = message
-        kwargs["extensions"] = extensions
-        super().__init__(*args, **kwargs)
-
-
-class GraphQLPermissionDenied(BaseGraphQLError):
-    """Permission denied error."""
-
-    _default_message: str = _(
-        "You do not have permission to perform this action",
-    )
-    _default_extensions: Optional[Dict[str, str]] = {
-        "code": ACCESS_DENIED,
-    }
-
-
-class GraphQLNotFound(BaseGraphQLError):
-    """Not found error."""
-
-    _default_message = _("Not found")
-    _default_extensions = {
-        "code": NOT_FOUND,
-    }
 
 
 class GraphQLInputError(BaseGraphQLError):
@@ -109,12 +64,3 @@ class GraphQLInputError(BaseGraphQLError):
                 converted_msgs.append(message)
 
         return converted_msgs
-
-
-class GraphQLAuthenticationFailed(BaseGraphQLError):
-    """Authentication failed error."""
-
-    _default_message = _("MSG_UNABLE_TO_LOGIN_WITH_PROVIDED_CREDENTIALS")
-    _default_extensions = {
-        "code": AUTHENTICATION_FAILED,
-    }
