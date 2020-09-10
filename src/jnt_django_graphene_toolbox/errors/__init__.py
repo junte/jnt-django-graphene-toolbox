5dc03e0dab685ab26cb59debb887c5ed4138b258
Stanislav Lapshin <staslapshin88@gmail.com>
2020-09-10 14:59:25 +0300

    [#] Add ghl backend

--- src/jnt_django_graphene_toolbox/errors/__init__.py
+++ src/jnt_django_graphene_toolbox/errors/__init__.py
@@ -4,3 +4,4 @@ from .authentication_failed import GraphQLAuthenticationFailed
 from .input import GraphQLInputError
 from .not_found import GraphQLNotFound
 from .permission_denied import GraphQLPermissionDenied
+from .base import BaseGraphQLError
