96826eaaca3c88f8628bac022bdbad404fb6dfca
Andrey Surovov <andrey.surovov@gmail.com>
2020-12-03 11:31:51 +0300

    Add EnumMultipleFilter. Updated "jnt-django-toolbox" Updated version: `0.1.23` -> `0.1.24` #7

---
+++ src/jnt_django_graphene_toolbox/filters/enum_multiple.py
@@ -0,0 +1,14 @@
+from jnt_django_graphene_toolbox.filters.strings_array import StringsArrayFilter
+
+from jnt_django_toolbox.forms.fields import MultipleEnumChoiceField
+
+
+class EnumMultipleFilter(StringsArrayFilter):
+    """Enum multiple filter."""
+
+    field_class = MultipleEnumChoiceField
+
+    def __init__(self, *args, **kwargs) -> None:
+        """Init enum multiple filter."""
+        kwargs.setdefault("lookup_expr", "in")
+        super().__init__(*args, **kwargs)
