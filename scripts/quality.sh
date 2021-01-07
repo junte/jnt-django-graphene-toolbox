bdc53b2074db7f3e4569872bcb494bfb7351c836
Stanislav Lapshin <sm.lapshin@gmail.com>
2021-01-07 16:26:37 +0300

    Add ignore to xenon

--- scripts/quality.sh
+++ scripts/quality.sh
@@ -13,7 +13,7 @@ run_checkers() {
   xenon --max-absolute A \
         --max-modules A \
         --max-average A \
-        --exclude src/jnt_django_graphene_toolbox/helpers/values.py \
+        --exclude src/jnt_django_graphene_toolbox/helpers/values.py,src/jnt_django_graphene_toolbox/fields/model_connection.py,src/jnt_django_graphene_toolbox/types/model.py \
         .
 
   # Checking `pyproject.toml` file contents:
