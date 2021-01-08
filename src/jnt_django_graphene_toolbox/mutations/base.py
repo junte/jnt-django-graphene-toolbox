bce609cf771a24aa7a8f976f17118e7ba96e9554
Stanislav Lapshin <sm.lapshin@gmail.com>
2021-01-08 12:07:34 +0300

    Fix base mutation class

--- src/jnt_django_graphene_toolbox/mutations/base.py
+++ src/jnt_django_graphene_toolbox/mutations/base.py
@@ -1,12 +1,27 @@
+import sys
 from typing import Optional
 
 import graphene
-from graphene.types.mutation import MutationOptions
+from graphene.types import mutation
 from graphql import ResolveInfo
+
+from jnt_django_graphene_toolbox.errors import (
+    BaseGraphQLError,
+    GraphQLPermissionDenied,
+)
+
+
+class MutationOptions(mutation.MutationOptions):
+    """Base mutation options."""
+
+    auth_required = None
 
 
 class BaseMutation(graphene.Mutation):
     """A base class mutation."""
+
+    class Meta:
+        abstract = True
 
     @classmethod
     def __init_subclass_with_meta__(  # noqa: WPS211
@@ -23,6 +38,21 @@ class BaseMutation(graphene.Mutation):
         super().__init_subclass_with_meta__(_meta=_meta, **options)
 
     @classmethod
+    def mutate(cls, root, info, **kwargs):  # noqa: WPS110
+        """Mutate."""
+        if not cls.check_premissions(root, info, **kwargs):
+            return GraphQLPermissionDenied()
+
+        try:
+            return cls.mutate_and_get_payload(root, info, **kwargs)
+        except Exception as err:
+            payload = cls.handle_error(root, info, err)
+            if payload:
+                return payload
+
+            raise
+
+    @classmethod
     def check_premissions(
         cls,
         root: Optional[object],
@@ -35,3 +65,25 @@ class BaseMutation(graphene.Mutation):
             return user.is_authenticated
 
         return True
+
+    @classmethod
+    def handle_error(
+        cls,
+        root: Optional[object],
+        info: ResolveInfo,  # noqa: WPS110,
+        error: Exception,
+    ):
+        """Handle error."""
+        if isinstance(error, BaseGraphQLError):
+            error.stack = sys.exc_info()[2]
+            return error
+
+    @classmethod
+    def mutate_and_get_payload(
+        cls,
+        root: Optional[object],
+        info: ResolveInfo,  # noqa: WPS110,
+        **kwargs,
+    ) -> None:  # noqa: WPS110
+        """Method should be implemented in subclasses."""
+        raise NotImplementedError
