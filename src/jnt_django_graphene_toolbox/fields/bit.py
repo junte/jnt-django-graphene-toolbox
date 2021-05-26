7938f6291c654236f5e53816abdf269247cb9bed
Stanislav Lapshin <sm.lapshin@gmail.com>
2021-05-26 17:49:23 +0300

    Refactor BitField

--- src/jnt_django_graphene_toolbox/fields/bit.py
+++ src/jnt_django_graphene_toolbox/fields/bit.py
@@ -1,10 +1,19 @@
 import graphene
+from graphene.utils.str_converters import to_snake_case
 
 
-class BitField(graphene.Scalar):
+class BitField(graphene.List):
     """Bit field."""
 
-    @staticmethod
-    def serialize(bit):  # noqa: WPS602
-        """Serialize."""
-        return [key for key, setted in bit if setted]
+    def __init__(self, enum, *args, **kwargs):
+        self._resolver = kwargs.get("resolver")
+        kwargs["resolver"] = self._resolver_wrap
+        super().__init__(graphene.Enum.from_enum(enum), *args, **kwargs)
+
+    def _resolver_wrap(self, instance, info):
+        if self._resolver:
+            values = self._resolver(instance, info)
+        else:
+            values = getattr(instance, to_snake_case(info.field_name))
+
+        return [key for key, setted in values if setted]
